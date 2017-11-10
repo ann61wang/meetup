f30e6a94971035f39a54940b3981b4e307dc4218
wangjingru <w15673142360@gmail.com>
2017-11-10 12:38:23 +0800

    Implement meetups#create

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -6,4 +6,17 @@ class MeetupsController < ApplicationController
   def new
     @meetup = Meetup.new
   end
+
+  def create
+    @meetup = Meetup.new(meetup_params)
+    @meetup.save
+
+    redirect_to meetups_path
+  end
+
+  private
+
+  def meetup_params
+    params.require(:meetup).permit(:title, :description)
+  end
 end
--- app/helpers/meetup_helper.rb
+++
@@ -1,2 +0,0 @@
-module MeetupHelper
-end
